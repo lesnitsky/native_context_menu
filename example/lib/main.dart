95a6afcc253534a1b08d4bfa3b6cbe01b71e71e6
LiJianying <lijy91@foxmail.com>
2022-02-21 18:29:53 +0800

    Fix Wrong position when hide titlebar. #13 (#14)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,11 @@
-import 'package:native_context_menu/native_context_menu.dart';
 import 'package:flutter/material.dart';
+import 'package:native_context_menu/native_context_menu.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
   runApp(const App());
 }
 
@@ -38,8 +42,45 @@ class _AppState extends State<App> {
             MenuItem(title: 'Fourth item'),
           ],
           child: Card(
-            child: Center(
-              child: Text(action ?? 'Right click me'),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                DragToMoveArea(
+                  child: Container(
+                    width: 300,
+                    height: 52,
+                    alignment: Alignment.center,
+                    margin: const EdgeInsets.all(20),
+                    color: Colors.grey.withOpacity(0.3),
+                    child: const Text('Drag to move window'),
+                  ),
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text('TitleBarStyle: '),
+                    TextButton(
+                      onPressed: () {
+                        windowManager.setTitleBarStyle('default');
+                      },
+                      child: const Text('default'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        windowManager.setTitleBarStyle('hidden');
+                      },
+                      child: const Text('hidden'),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: Center(
+                    child: Text(action ?? 'Right click me'),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -7,8 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <native_context_menu/native_context_menu_plugin.h>
+#include <window_manager/window_manager_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   NativeContextMenuPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("NativeContextMenuPlugin"));
+  WindowManagerPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowManagerPlugin"));
 }
